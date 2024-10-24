b5dd8c3a48db8ccb31c38c07e2b6393b4ed59369
Oliviaa7 <olivialanderson1994@gmail.com>
2024-10-24 13:55:33 -0700

    added 'say: ' keys

--- input.js
+++ input.js
@@ -28,6 +28,21 @@ const handleUserInput = function (key) {
     case "d":
       connection.write("Move: right");
       break;
+    case "i":
+      connection.write("Say: I dunno");
+      break;
+    case "g":
+      connection.write("Say: GOTCHA");
+      break;
+    case "h":
+      connection.write("Say: HA!");
+      break;
+    case "t":
+      connection.write("Say: Time to go!");
+      break;
+    case "b":
+      connection.write("Say: Goodbye now!");
+      break;
     case "\u0003":
       process.exit();
   }
